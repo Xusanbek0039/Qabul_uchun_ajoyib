0967ec72ba0094d7e852ab1ef4e57ae9b97def8c
Snouden0071 <144625437+bekzodsindorov1524@users.noreply.github.com>
2023-09-17 04:15:26 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -127,13 +127,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
