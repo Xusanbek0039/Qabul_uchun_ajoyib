de48123e2c97369eb573c32cf62bfeb33331d7f2
Snouden0071 <144625437+bekzodsindorov1524@users.noreply.github.com>
2023-09-17 03:50:52 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-MEDIA_URL = '/static/videos/'
+MEDIA_URL = '/media/'
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
