7d07d9f84ccb1f67220bc5ecfbeb2ba3ee11cd45
Snouden0071 <144625437+bekzodsindorov1524@users.noreply.github.com>
2023-09-17 04:27:30 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
-
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
