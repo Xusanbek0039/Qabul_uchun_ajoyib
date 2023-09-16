4818611aab8a2aa4683fa26951fb659b42f69983
Snouden0071 <144625437+bekzodsindorov1524@users.noreply.github.com>
2023-09-17 01:52:42 +0500

    Initial commit

---
+++ myapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',HomePageView.as_view(),name='index'),
+    path('contact/',ContacView.as_view(),name='contact'),
+    path('login/',Loginview.as_view(),name='login'),
+    path('error/',ErrorView.as_view(),name='404'),
+    path('pasrest/',PasresertView.as_view(),name='password-reset'),
+    path('register/',Registerview.as_view(),name='register'),
+    path('soat/',SoatView.as_view(),name='soat'),
+
+    
+]
---
+++ myapp/views.py
@@ -0,0 +1,32 @@
+
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'index.html'
+
+
+class ContacView(TemplateView):
+    template_name = 'contact.html'
+
+
+class Loginview(TemplateView):
+    template_name = 'login.html'
+
+
+class ErrorView(TemplateView):
+    template_name = '404.html'
+
+
+class PasresertView(TemplateView):
+    template_name = 'password-reset.html'
+
+
+class Registerview(TemplateView):
+    template_name='register.html'
+
+class SoatView(TemplateView):
+    template_name='soat.html'
