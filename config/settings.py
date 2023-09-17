5814f02f974b8c7cfd50e2d5f54fd70fc50e7a36
Snouden0071 <144625437+bekzodsindorov1524@users.noreply.github.com>
2023-09-17 05:19:26 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
